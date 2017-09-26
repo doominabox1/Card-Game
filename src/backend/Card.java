af84cac3e8a309128e38acab0a8d3552b9cf4d23
mattj122 <mattj122@users.noreply.github.com>
2017-09-26 12:58:53 -0500

    Tracking sheets

--- src/backend/Card.java
+++ src/backend/Card.java
@@ -1,5 +1,46 @@
 package backend;
 
-public class Card {
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
+
+public class Card {
+	
+	int pos;
+	
+	BufferedImage faces = null; 
+	
+	public void loadImage() {
+		try {
+			faces = ImageIO.read(new File("cards.jpg"));
+		}
+		catch (IOException e){
+			
+		}
+	}
+	
+	public Card(int pos) {
+		this.pos = pos;
+	}
+	
+	public Card() {
+		pos = 0;
+	}
+	
+	public int getSuit() {
+		return (pos/13);
+	}
+	public int getValue() {
+		return (pos % 13 + 2);
+	}
+	public void setValue(int pos) {
+		this.pos = pos;
+	}
+	/*
+	public BufferedImage getCardImage() {
+		
+	}
+	*/
 }
